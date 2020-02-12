19abea0fcd0f393c29200d1bda66e045b7dc6c51
thomaspuppe <info@thomaspuppe.de>
2020-02-12 12:32:54 +0100

    be defensive

--- payment.js
+++ payment.js
@@ -1,5 +1,5 @@
 (function () {
-    var resultText = PaymentRequest ? 'AVAILABLE! 💸' : 'NOT AVAILABLE</p>';
+    var resultText = typeof PaymentRequest !== 'undefined' ? 'AVAILABLE! 💸' : 'NOT AVAILABLE</p>';
     document.querySelector('main').insertAdjacentHTML('beforeend', '<article><h3>PaymentRequest API</h3><p>' + resultText + '</p></article>');
 }());
 
