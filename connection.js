eb5d16ebc408cc26bd34979922da213894e62a09
thomaspuppe <info@thomaspuppe.de>
2020-02-12 12:25:11 +0100

    add two new APIs: battery and payment

--- connection.js
+++ connection.js
@@ -1,2 +1,4 @@
-var resultText = navigator.connection ? navigator.connection.toString() : 'navigator.connection IS NOT AVAILABLE';
-document.querySelector('main').insertAdjacentHTML('beforeend', '<article><h3>Connection?</h3> ' + resultText + '<article>');
+(function () {
+    var resultText = navigator.connection ? '<pre>' + navigator.connection.toString() + '</pre>' : '<p>navigator.connection IS NOT AVAILABLE</p>';
+    document.querySelector('main').insertAdjacentHTML('beforeend', '<article><h3>Connection?</h3> ' + resultText + '</article>');
+}());
